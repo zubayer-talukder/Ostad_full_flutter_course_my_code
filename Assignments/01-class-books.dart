ccd76e1f1ebe01a83a9d029e4a55e8a4269e37c6
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-09-24 09:58:53 +0600

    assignment Variable decleration Done

--- Assignments/01-class-books.dart
+++ Assignments/01-class-books.dart
@@ -1,4 +1,12 @@
+class book {
+  String?
+      title; // null safety ?--> hoise it means that i will ensure that i'm going to provide a value later
+  String? author;
+  int? publication_year;
+  int? pagesRead;
+  
+}
+
 void main() {
   print("ping!!");
-  
 }
