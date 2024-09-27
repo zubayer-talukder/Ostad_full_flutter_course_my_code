53125b0682b4d3c8d057757062babe27da279cf5
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-09-27 23:38:50 +0600

    Completed

---
+++ live test/1-livetest.dart
@@ -0,0 +1,24 @@
+class Car {
+  String? brand;
+  String? Model;
+  int? year;
+  int? car_age;
+
+  //constructor
+  Car(this.brand, this.Model, this.year) {}
+
+  //method
+  int carAge() {
+    return DateTime.now().year - year!;
+  }
+}
+
+main() {
+  var car1 = Car("Toyota", "Corolla", 2015);
+  car1.car_age = car1.carAge();
+
+  print("Brand: ${car1.brand}");
+  print("\nModel: ${car1.Model}");
+  print("\nYear: ${car1.year}");
+  print("\nCar Age: ${car1.carAge()} years. ");
+}
